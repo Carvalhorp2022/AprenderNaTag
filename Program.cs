86105e17edf5332eaf58ff16fddde2e499693070
Rodrigo Carvalho <113652984+Carvalhorp2022@users.noreply.github.com>
2023-03-20 11:22:29 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
+
 Console.WriteLine("boa");
-=======
 Console.WriteLine("Ola Rider");
->>>>>>> 190bee111e81cd920de12832f15c4c9bd2c1beff
 
